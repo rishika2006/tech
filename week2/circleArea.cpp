855b4a56803a11e4bf5d085ae74c56ae8406cdf6
rishika2006 <166004746+rishika2006@users.noreply.github.com>
2024-09-27 18:42:45 +0530

    week 2

---
+++ week2/circleArea.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+   cout<<"enter the radius of a circle:";
+   float radius,area;
+   cin>>radius;
+   radius=3.14*radius*radius;
+   cout<<"radius of circle is "<<radius<<endl;
+  return 0;
+}
