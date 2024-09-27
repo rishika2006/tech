7c8b11425cf04a6d208575a15e3254b349b026f5
rishika2006 <166004746+rishika2006@users.noreply.github.com>
2024-09-27 18:20:37 +0530

    updated

---
+++ week 1/percentage.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+   cout<<"enter the marks of hindi:";
+   int hindi,english,math,science,computer;
+   cin>>hindi;
+   cout<<"enter the marks of english:";
+   cin>>english;
+   cout<<"enter the marks of math :";
+   cin>>math;
+   cout<<"enter the marks of science:";
+   cin>>science;
+   cout<<"enter the marks of computer:";
+   cin>>computer;
+   float percentage=(hindi+english+math+science+computer)*100/500;
+   cout<<"percentage="<<percentage<<endl;
+   return 0;
+
+}
