96004ba819d7777713138b93e5124d28d28bc6d9
rishika2006 <166004746+rishika2006@users.noreply.github.com>
2024-09-29 18:29:17 +0530

    updated

---
+++ week2/even.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"enter a positive integer:";
+int num;
+cin>>num;
+if(num%2==0){
+    cout<<"number is even";
+}
+else{
+    cout<<"number is odd";
+}
+    return 0;
+}
