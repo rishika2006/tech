444fe7a0d5ffe2754b3b15963f4e1dcdb1d8fb6b
rishika2006 <166004746+rishika2006@users.noreply.github.com>
2024-09-29 18:13:53 +0530

    updated

---
+++ week2/circumference.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+   float radius=10.0;
+   float cir=2*3.14*radius;
+   cout<<"circumference of circle having radius 10 is "<<cir;
+    return 0;
+}
