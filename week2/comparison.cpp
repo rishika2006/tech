de5b74899f0585e12c8cfd1d0d302da58abd1b4b
rishika2006 <166004746+rishika2006@users.noreply.github.com>
2024-09-29 18:46:54 +0530

    updated

---
+++ week2/comparison.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"enter the radius of the circle:";
+int radius;
+cin>>radius;
+float area=3.14*radius*radius;
+float cir=2*3.14*radius;
+cout<<"area="<<area<<endl;
+cout<<"circumference="<<cir<<endl;
+if(area>cir){
+    cout<<"area of circle having radius "<<radius<<" is larger than its circumference";
+}
+else{
+        cout<<"area of circle having radius "<<radius<<"is smaller than its circumference";
+
+   }
+
+    return 0;
+}
